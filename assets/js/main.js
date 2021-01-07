5da686191c5890c5ac6f3964cab7a25c13775a61
Mohamed Ait Si Mhand <mohamedaitsimhand@gmail.com>
2021-01-07 19:53:37 +0100

    Special Products section is ready Deals that Delight section in progress

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,32 @@
 $(() => {
 	// Enable Bootstrap tooltip
 	$('[data-toggle="tooltip"]').tooltip();
+	// Special Products Carousel
+	$('.special-product__carousel').owlCarousel({
+		margin: 22,
+		items: 4,
+		autoWidth: true,
+		navText: ['<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L1 7L7 1" stroke="#4AAE34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>', '<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 13L7 7L1 1" stroke="#4AAE34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'],
+		responsive: {
+			0: {
+				nav: false
+			},
+			992: {
+				nav: true
+			}
+		}
+	})
+	// Deals that Delight Carousel
+	$('.deals__carousel').owlCarousel({
+		items: 1,
+		navText: ['<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L1 7L7 1" stroke="#4AAE34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>', '<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 13L7 7L1 1" stroke="#4AAE34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'],
+		responsive: {
+			0: {
+				nav: false
+			},
+			992: {
+				nav: true
+			}
+		}
+	});
 });
-
-// Ripple Button Effect, Get clicked x and y axise and feed the value to css variables (--ripple-x, --ripple-y)
-// const root = document.documentElement;
-// document.addEventListener("mousedown", ev => {
-// 	const el = ev.target,
-// 		top = 0,
-// 		left = 0;
-// 	do {
-// 		top += el.offsetTop;
-// 		left += el.offsetLeft;
-// 	} while (el = el.offsetParent);
-// 	const x = (ev.clientX - left) / ev.target.offsetWidth;
-// 	const y = (ev.clientY - top) / ev.target.offsetHeight;
-// 	root.style.setProperty("--ripple-x", x);
-// 	root.style.setProperty("--ripple-y", y);
-// });
