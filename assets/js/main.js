11a18b312b1dad90bc7cfcaf94c40c5b52137a46
Mohamed Ait Si Mhand <mohamedaitsimhand@gmail.com>
2021-01-09 15:53:35 +0100

    testimonials section is ready

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,12 @@
 $(() => {
 	// Enable Bootstrap tooltip
 	$('[data-toggle="tooltip"]').tooltip();
+	
 	// Special Products Carousel
 	$('.special-product__carousel').owlCarousel({
 		margin: 22,
 		items: 4,
+		dots: false,
 		autoWidth: true,
 		navText: ['<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L1 7L7 1" stroke="#4AAE34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>', '<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 13L7 7L1 1" stroke="#4AAE34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'],
 		responsive: {
@@ -16,10 +18,12 @@ $(() => {
 			}
 		}
 	})
+
 	// Deals that Delight Carousel
 	$('.deals__carousel').owlCarousel({
 		margin: 24,
 		items: 1,
+		dots: false,
 		navText: ['<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L1 7L7 1" stroke="#4AAE34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>', '<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 13L7 7L1 1" stroke="#4AAE34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'],
 		responsive: {
 			0: {
@@ -32,6 +36,7 @@ $(() => {
 			}
 		}
 	});
+
 	// Team Section Carousel
 	$('.team__carousel').owlCarousel({
 		margin: 24,
@@ -40,5 +45,14 @@ $(() => {
 		dots: false,
 		navText: ['<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L1 7L7 1" stroke="#4AAE34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>', '<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 13L7 7L1 1" stroke="#4AAE34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>'],
 		animateOut: 'fadeOut'
+	});
+
+	// Testimonials sSection Carousel
+	$('.testimonials__carousel').owlCarousel({
+		margin: 118,
+		nav: true,
+		items: 2,
+		dots: false,
+		navText: ['<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M7 13L1 7L7 1" stroke="#4AAE34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>', '<svg width="8" height="14" viewBox="0 0 8 14" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M1 13L7 7L1 1" stroke="#4AAE34" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/></svg>']
 	});
 });
